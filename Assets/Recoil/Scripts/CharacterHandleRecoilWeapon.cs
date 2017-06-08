a1b137ed1c938a7e55bc6865792ab7cd818bc8bc
Terje Ballestad <terj.ba@gmail.com>
2017-06-08 20:06:06 +0200

    (recoil doesnt work)

---
+++ Assets/Recoil/Scripts/CharacterHandleRecoilWeapon.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.CorgiEngine;
+
+public class CharacterHandleRecoilWeapon : CharacterHandleWeapon {
+
+
+}
